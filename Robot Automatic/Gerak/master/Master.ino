c8bc04204eeded0cd837b9eec3a90104c5fc6ea5
BRIDOWCT <aburoboconunair@gmail.com>
2026-05-29 14:55:44 +0700

    Update Master.ino

--- Robot Automatic/Gerak/master/Master.ino
+++ Robot Automatic/Gerak/master/Master.ino
@@ -1,208 +1,64 @@
-#include <Wire.h>
-
-#define RXD2 16
-#define TXD2 17
-
-#define RS485_DIR 4
-
-enum RobotState {
-  MOVE_FORWARD,
-  STOP_1,
-  STRAFE_LEFT,
-  STOP_ALL
-};
-
-RobotState state = MOVE_FORWARD;
-
-float posX = 0;
-float posY = 0;
-float yaw = 0;
-
-float targetForward = 1.0;
-float targetLeft = 2.0;
-
-String buffer = "";
+#define RS485_EN 4
 
 void setup() {
 
   Serial.begin(115200);
 
-  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  pinMode(RS485_EN, OUTPUT);
 
-  pinMode(RS485_DIR, OUTPUT);
-
-  digitalWrite(RS485_DIR, LOW);
-
-  Serial.println("MASTER READY");
+  digitalWrite(RS485_EN, LOW);
 }
 
 void loop() {
 
-  receiveData();
+  float angle1 = RequestData("1");
 
-  stateRobot();
+  delay(10);
 
-  debugMonitor();
+  float angle2 = RequestData("2");
 
-  delay(20);
+  delay(10);
+
+  Serial.print("AS5600_1 : ");
+  Serial.print(angle1);
+
+  Serial.print(" || AS5600_2 : ");
+  Serial.println(angle2);
+
+  delay(100);
 }
 
-void stateRobot() {
+float RequestData(String address) {
 
-  switch(state) {
+  String received = "";
 
-    case MOVE_FORWARD:
+  digitalWrite(RS485_EN, HIGH);
 
-      if(posY < targetForward) {
+  Serial.print(address);
+  Serial.print("\n");
 
-        mecanumMove(120,120,120,120);
+  Serial.flush();
 
-      } else {
+  delay(2);
 
-        stopAll();
+  digitalWrite(RS485_EN, LOW);
 
-        delay(1000);
+  unsigned long timeout = millis();
 
-        state = STRAFE_LEFT;
+  while (millis() - timeout < 100) {
+
+    while (Serial.available()) {
+
+      char c = Serial.read();
+
+      if (c == '\n') {
+
+        return received.toFloat();
       }
 
-    break;
-
-    case STRAFE_LEFT:
-
-      if(abs(posX) < targetLeft) {
-
-        mecanumMove(-120,120,120,-120);
-
-      } else {
-
-        stopAll();
-
-        state = STOP_ALL;
-      }
-
-    break;
-
-    case STOP_ALL:
-
-      stopAll();
-
-    break;
-  }
-}
-
-void mecanumMove(int lf, int rf, int lb, int rb) {
-
-  float correction = yaw * 5.0;
-
-  lf -= correction;
-  rf += correction;
-
-  lb -= correction;
-  rb += correction;
-
-  sendFront(lf, rf);
-
-  delay(2);
-
-  sendBack(lb, rb);
-}
-
-void stopAll() {
-
-  sendFront(0,0);
-
-  delay(2);
-
-  sendBack(0,0);
-}
-
-void sendFront(int lf, int rf) {
-
-  digitalWrite(RS485_DIR, HIGH);
-
-  String data =
-    "F," +
-    String(lf) + "," +
-    String(rf) + "\n";
-
-  Serial2.print(data);
-
-  delay(2);
-
-  digitalWrite(RS485_DIR, LOW);
-}
-
-void sendBack(int lb, int rb) {
-
-  digitalWrite(RS485_DIR, HIGH);
-
-  String data =
-    "B," +
-    String(lb) + "," +
-    String(rb) + "\n";
-
-  Serial2.print(data);
-
-  delay(2);
-
-  digitalWrite(RS485_DIR, LOW);
-}
-
-void receiveData() {
-
-  while(Serial2.available()) {
-
-    char c = Serial2.read();
-
-    if(c == '\n') {
-
-      parsing(buffer);
-
-      buffer = "";
-    }
-    else {
-
-      buffer += c;
+      received += c;
     }
   }
-}
 
-void parsing(String data) {
-
-  if(data.startsWith("X")) {
-
-    int p1 = data.indexOf(',');
-
-    posX = data.substring(p1 + 1).toFloat();
-  }
-
-  else if(data.startsWith("Y")) {
-
-    int p1 = data.indexOf(',');
-
-    posY = data.substring(p1 + 1).toFloat();
-  }
-
-  else if(data.startsWith("I")) {
-
-    int p1 = data.indexOf(',');
-
-    yaw = data.substring(p1 + 1).toFloat();
-  }
-}
-
-void debugMonitor() {
-
-  Serial.print("X : ");
-  Serial.print(posX);
-
-  Serial.print(" | Y : ");
-  Serial.print(posY);
-
-  Serial.print(" | Yaw : ");
-  Serial.print(yaw);
-
-  Serial.print(" | State : ");
-  Serial.println(state);
+  return 0;
 }
