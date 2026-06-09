d3fef0f194e1f9c8b2a4c5b338232ba7e92ae18b
BRIDOWCT <aburoboconunair@gmail.com>
2026-06-09 16:51:39 +0700

    Update Encoderx.ino

--- Robot Automatic/Gerak/Slave/Encoderx.ino
+++ Robot Automatic/Gerak/Slave/Encoderx.ino
@@ -1,143 +1,161 @@
 #include <Wire.h>
+#include <ModbusRtu.h>
 
-#define AS5600 0x36
 
-#define RS485_EN 4
+#define SLAVE_ID 1 //1 X,2y
+#define EN_PIN   4
 
-int Highbyte;
-int Lowbyte;
+#define AS5600_ADDR 0x36
 
-uint16_t raw_angle;
+//roda bantu (cm)
+#define DIAMETER 5.0
+#define PI_VALUE 3.14159265
 
-float angle;
-float set_angle;
+//com
+uint16_t holdingRegs[2];
 
-int kuadran;
-int last_kuadran;
+Modbus slave(SLAVE_ID, Serial, EN_PIN);
 
-bool first_condition = true;
+//var
+uint16_t rawAngle = 0;
 
-int rotation;
-float total_angle;
+float angle = 0.0;
+float startAngle = 0.0;
 
-String incoming = "";
+int quadrant = 0;
+int lastQuadrant = 0;
 
-void setup() {
+bool firstRead = true;
 
-  pinMode(RS485_EN, OUTPUT);
-  digitalWrite(RS485_EN, LOW);
+long rotationCount = 0;
 
+float totalAngle = 0.0;
+float distance = 0.0;
+
+float readAS5600()
+{
+  Wire.beginTransmission(AS5600_ADDR);
+  Wire.write(0x0C); // RAW ANGLE HIGH BYTE
+  Wire.endTransmission();
+
+  Wire.requestFrom(AS5600_ADDR, 2);
+
+  if (Wire.available() >= 2)
+  {
+    uint8_t highByte = Wire.read();
+    uint8_t lowByte  = Wire.read();
+
+    rawAngle = ((highByte << 8) | lowByte) & 0x0FFF;
+
+    return rawAngle * 360.0 / 4096.0;
+  }
+
+  return angle;
+}
+
+void correctAngle()
+{
+  angle -= startAngle;
+
+  if (angle < 0)
+  {
+    angle += 360.0;
+  }
+}
+
+void calculateRotation()
+{
+  if (angle >= 0 && angle <= 90)
+  {
+    quadrant = 1;
+  }
+  else if (angle > 90 && angle <= 180)
+  {
+    quadrant = 2;
+  }
+  else if (angle > 180 && angle <= 270)
+  {
+    quadrant = 3;
+  }
+  else
+  {
+    quadrant = 4;
+  }
+
+  if (firstRead)
+  {
+    lastQuadrant = quadrant;
+    firstRead = false;
+  }
+
+  // CW
+  if (quadrant == 1 && lastQuadrant == 4)
+  {
+    rotationCount++;
+  }
+
+  // CCW
+  else if (quadrant == 4 && lastQuadrant == 1)
+  {
+    rotationCount--;
+  }
+
+  lastQuadrant = quadrant;
+
+  totalAngle = (rotationCount * 360.0) + angle;
+}
+
+void calculateDistance()
+{
+  distance =
+      ((DIAMETER * PI_VALUE) / 360.0) *
+      totalAngle;
+}
+
+//register 
+void updateModbusRegisters()
+{
+  union
+  {
+    float value;
+    uint16_t reg[2];
+  } converter;
+
+  converter.value = distance;
+
+  holdingRegs[0] = converter.reg[0];
+  holdingRegs[1] = converter.reg[1];
+}
+
+void setup()
+{
   Serial.begin(115200);
 
   Wire.begin();
 
-  delay(1000);
+  pinMode(EN_PIN, OUTPUT);
+  digitalWrite(EN_PIN, LOW);
 
-  ReadAngle();
-  set_angle = angle;
+  slave.start();
+
+  delay(500);
+
+  startAngle = readAS5600();
+
+  Serial.println("AS5600 MODBUS SLAVE READY");
 }
 
-void loop() {
+void loop()
+{
+  angle = readAS5600();
 
-  ReadAngle();
-  CorrectAngle();
-  Rotation();
+  correctAngle();
 
-  ReceiveCommand();
-}
+  calculateRotation();
 
-void ReceiveCommand() {
+  calculateDistance();
 
-  while (Serial.available()) {
+  updateModbusRegisters();
 
-    char c = Serial.read();
-
-    if (c == '\n') {
-
-      incoming.trim();
-// kondisi kirim data,1 buat x
-      if (incoming == "1") {
-        SendData();
-      }
-
-      incoming = "";
-    }
-    else {
-      incoming += c;
-    }
-  }
-}
-
-void SendData() {
-
-  digitalWrite(RS485_EN, HIGH);
-
-  Serial.print(total_angle);
-  Serial.print("\n");
-
-  Serial.flush();
-
-  delay(2);
-
-  digitalWrite(RS485_EN, LOW);
-}
-
-void ReadAngle() {
-
-  Wire.beginTransmission(AS5600);
-  Wire.write(0x0E);
-  Wire.endTransmission();
-
-  Wire.requestFrom(AS5600, 2);
-
-  if (Wire.available() == 2) {
-
-    Highbyte = Wire.read();
-    Lowbyte = Wire.read();
-
-    raw_angle = (((Highbyte << 8) | Lowbyte) & 0x0FFF);
-
-    angle = raw_angle * 360.0 / 4096.0;
-  }
-}
-
-void CorrectAngle() {
-
-  angle = angle - set_angle;
-
-  if (angle < 0) {
-    angle += 360;
-  }
-}
-
-void Rotation() {
-
-  if ((angle >= 0) && (angle <= 90)) {
-    kuadran = 1;
-  }
-  else if ((angle > 90) && (angle <= 180)) {
-    kuadran = 2;
-  }
-  else if ((angle > 180) && (angle <= 270)) {
-    kuadran = 3;
-  }
-  else {
-    kuadran = 4;
-  }
-
-  if (first_condition) {
-    last_kuadran = kuadran;
-    first_condition = false;
-  }
-
-  if ((kuadran == 1) && (last_kuadran == 4)) {
-    rotation++;
-  }
-  else if ((kuadran == 4) && (last_kuadran == 1)) {
-    rotation--;
-  }
-
-  last_kuadran = kuadran;
-
-  total_angle = rotation * 360 + angle;
+  slave.poll(holdingRegs, 2);
 }
