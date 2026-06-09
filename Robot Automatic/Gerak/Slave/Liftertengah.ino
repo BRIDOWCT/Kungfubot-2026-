c3ba5772931e2b93bbdfb1333e4654f6c07078c7
BRIDOWCT <aburoboconunair@gmail.com>
2026-06-09 16:54:49 +0700

    Create Liftertengah.ino

---
+++ Robot Automatic/Gerak/Slave/Liftertengah.ino
@@ -0,0 +1,205 @@
+#include <ModbusRtu.h>
+
+#define SLAVE_ID 11
+#define EN_PIN   4
+
+// Encoder
+#define ENC_A 2
+#define ENC_B 3
+
+// BTS7960
+#define MOTOR_P 5
+#define MOTOR_N 6
+
+#define TARGET_UP_TICK   -3000
+#define TARGET_DOWN_TICK 0
+
+Modbus slave(SLAVE_ID, Serial, EN_PIN);
+
+uint16_t holdingRegs[3];
+
+volatile long encoderTicks = 0;
+
+enum LiftState
+{
+  IDLE,
+  MOVING_UP,
+  MOVING_DOWN,
+  FINISHED
+};
+
+LiftState liftState = IDLE;
+
+// =====================================================
+// ENCODER
+// =====================================================
+
+void encoderA()
+{
+  bool A = digitalRead(ENC_A);
+  bool B = digitalRead(ENC_B);
+
+  if(A == B)
+    encoderTicks--;
+  else
+    encoderTicks++;
+}
+
+void encoderB()
+{
+  bool A = digitalRead(ENC_A);
+  bool B = digitalRead(ENC_B);
+
+  if(A != B)
+    encoderTicks++;
+  else
+    encoderTicks--;
+}
+
+// =====================================================
+// MOTOR
+// =====================================================
+
+void stopMotor()
+{
+  analogWrite(MOTOR_P,0);
+  analogWrite(MOTOR_N,0);
+}
+
+void moveUp()
+{
+  analogWrite(MOTOR_P,40);
+  analogWrite(MOTOR_N,0);
+}
+
+void moveDown()
+{
+  analogWrite(MOTOR_P,0);
+  analogWrite(MOTOR_N,40);
+}
+
+// =====================================================
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(MOTOR_P,OUTPUT);
+  pinMode(MOTOR_N,OUTPUT);
+
+  pinMode(ENC_A,INPUT_PULLUP);
+  pinMode(ENC_B,INPUT_PULLUP);
+
+  attachInterrupt(
+    digitalPinToInterrupt(ENC_A),
+    encoderA,
+    CHANGE);
+
+  attachInterrupt(
+    digitalPinToInterrupt(ENC_B),
+    encoderB,
+    CHANGE);
+
+  holdingRegs[0] = 0;
+  holdingRegs[1] = 0;
+  holdingRegs[2] = 0;
+
+  slave.start();
+
+  Serial.println("SLAVE 11 READY");
+}
+
+void loop()
+{
+  slave.poll(holdingRegs,3);
+
+  holdingRegs[1] = (int16_t)encoderTicks;
+
+  switch(liftState)
+  {
+    case IDLE:
+    {
+      holdingRegs[2] = 0;
+
+      if(holdingRegs[0] == 1)
+      {
+        liftState = MOVING_UP;
+
+        Serial.println("LIFT UP");
+      }
+
+      if(holdingRegs[0] == 2)
+      {
+        liftState = MOVING_DOWN;
+
+        Serial.println("LIFT DOWN");
+      }
+    }
+    break;
+
+    case MOVING_UP:
+    {
+      holdingRegs[2] = 1;
+
+      moveUp();
+
+      if(encoderTicks <= TARGET_UP_TICK)
+      {
+        stopMotor();
+
+        liftState = FINISHED;
+
+        Serial.println("UP FINISHED");
+      }
+    }
+    break;
+
+    case MOVING_DOWN:
+    {
+      holdingRegs[2] = 1;
+
+      moveDown();
+
+      if(encoderTicks >= TARGET_DOWN_TICK)
+      {
+        stopMotor();
+
+        liftState = FINISHED;
+
+        Serial.println("DOWN FINISHED");
+      }
+    }
+    break;
+
+    case FINISHED:
+    {
+      stopMotor();
+
+      holdingRegs[2] = 2;
+
+      holdingRegs[0] = 0;
+    }
+    break;
+  }
+
+  
+  static unsigned long t=0;
+
+  if(millis()-t > 500)
+  {
+    t=millis();
+
+    Serial.print("CMD=");
+    Serial.print(holdingRegs[0]);
+
+    Serial.print(" TICK=");
+    Serial.print(encoderTicks);
+
+    Serial.print(" STATUS=");
+    Serial.print(holdingRegs[2]);
+
+    Serial.print(" STATE=");
+    Serial.println(liftState);
+  }
+  
+}
