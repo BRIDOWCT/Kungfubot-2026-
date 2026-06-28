ac3635a0d27c8870c343b7374833e6ea4a81e265
BRIDOWCT <aburoboconunair@gmail.com>
2026-06-28 21:01:04 +0700

    Update GripperSpearhead.ino

--- Robot Automatic/Gerak/Slave/GripperSpearhead.ino
+++ Robot Automatic/Gerak/Slave/GripperSpearhead.ino
@@ -1,230 +1,70 @@
 #include <ModbusRtu.h>
 #include <Servo.h>
 
-#define SLAVE_ID 10
+#define SLAVE_ID 7
 #define EN_PIN   4
 
-// Encoder
-#define ENC_A 2
-#define ENC_B 3
-
-// BTS7960
-#define MOTOR_P 5
-#define MOTOR_N 6
-
-// Servo
-#define SERVO_ARM   9
-#define SERVO_GRIP  10
+#define SERVO1_PIN 10
+#define SERVO2_PIN 9
 
 Modbus slave(SLAVE_ID, Serial, EN_PIN);
 
-uint16_t holdingRegs[3];
+uint16_t holdingRegs[1];
 
-/*
-holdingRegs[0] = command
-
-0 = idle
-1 = scan
-2 = grip
-
-holdingRegs[1] = encoder tick
-
-holdingRegs[2] = status
-
-0 = idle
-1 = scanning
-2 = finished
-*/
-
-Servo servoArm;
-Servo servoGrip;
-
-volatile long encoderTicks = 0;
-
-enum ArmState
-{
-  IDLE,
-  SCANNING,
-  GRIPPING,
-  FINISHED
-};
-
-ArmState armState = IDLE;
-
-// =====================================================
-// ENCODER
-// =====================================================
-
-void encoderA()
-{
-  bool A = digitalRead(ENC_A);
-  bool B = digitalRead(ENC_B);
-
-  if (A == B)
-    encoderTicks--;
-  else
-    encoderTicks++;
-}
-
-void encoderB()
-{
-  bool A = digitalRead(ENC_A);
-  bool B = digitalRead(ENC_B);
-
-  if (A != B)
-    encoderTicks++;
-  else
-    encoderTicks--;
-}
-
-// =====================================================
-// MOTOR
-// =====================================================
-
-void stopMotor()
-{
-  analogWrite(MOTOR_P, 0);
-  analogWrite(MOTOR_N, 0);
-}
-
-// arah dibalik sesuai masalah yang Anda laporkan
-void moveScan()
-{
-  analogWrite(MOTOR_P, 15);
-  analogWrite(MOTOR_N, 0);
-}
-
-// =====================================================
+Servo angkat;
+Servo grip;
 
 void setup()
 {
   Serial.begin(115200);
 
-  pinMode(ENC_A, INPUT_PULLUP);
-  pinMode(ENC_B, INPUT_PULLUP);
+  angkat.attach(SERVO1_PIN);
+  grip.attach(SERVO2_PIN);
 
-  pinMode(MOTOR_P, OUTPUT);
-  pinMode(MOTOR_N, OUTPUT);
-
-  attachInterrupt(
-    digitalPinToInterrupt(ENC_A),
-    encoderA,
-    CHANGE);
-
-  attachInterrupt(
-    digitalPinToInterrupt(ENC_B),
-    encoderB,
-    CHANGE);
-
-  // servoArm.attach(SERVO_ARM);
-  servoGrip.attach(SERVO_GRIP);
-
-  // posisi awal
-
-  // servoArm.write(90);
-  servoGrip.write(90);
-
-  holdingRegs[0] = 0;
-  holdingRegs[1] = 0;
-  holdingRegs[2] = 0;
+  // Posisi awal
+  angkat.write(10);
+  grip.write(70);
 
   slave.start();
 
-  Serial.println("SLAVE 10 READY");
+  Serial.println("Servo Slave Ready");
 }
-
-// =====================================================
 
 void loop()
 {
-  slave.poll(holdingRegs, 3);
+  slave.poll(holdingRegs, 1);
 
-  holdingRegs[1] = (int16_t)encoderTicks;
+  static unsigned long lastPrint = 0;
 
-  switch(armState)
+  if (millis() - lastPrint > 500)
   {
-    // ==========================================
-    case IDLE:
-    {
-      holdingRegs[2] = 0;
+    lastPrint = millis();
 
-      if(holdingRegs[0] == 1)
-      {
-        armState = SCANNING;
-
-        Serial.println("START SCAN");
-      }
-    }
-    break;
-
-    // ==========================================
-    case SCANNING:
-    {
-      holdingRegs[2] = 1;
-
-      moveScan();
-
-      if(holdingRegs[0] == 2)
-      {
-        stopMotor();
-
-        armState = GRIPPING;
-
-        Serial.println("OBJECT DETECTED");
-      }
-    }
-    break;
-
-    // ==========================================
-    case GRIPPING:
-    {
-      Serial.println("GRIP");
-
-      servoGrip.write(0);
-
-      delay(1000);
-
-      Serial.println("ARM");
-
-      // servoArm.write(0);
-
-      delay(1000);
-
-      holdingRegs[2] = 2;
-
-      armState = FINISHED;
-    }
-    break;
-
-    // ==========================================
-    case FINISHED:
-    {
-      stopMotor();
-
-      holdingRegs[2] = 2;
-
-      // reset command supaya tidak retrigger
-      holdingRegs[0] = 0;
-    }
-    break;
+    Serial.print("CMD = ");
+    Serial.println(holdingRegs[0]);
   }
 
-  static unsigned long t = 0;
-
-  if(millis() - t > 500)
+  if (holdingRegs[0] == 1)
   {
-    t = millis();
+    Serial.println("grip");
 
-    Serial.print("CMD=");
-    Serial.print(holdingRegs[0]);
+    grip.write(0);
+    delay(300);
 
-    Serial.print(" STATE=");
-    Serial.print(armState);
+    angkat.write(90);
 
-    Serial.print(" STATUS=");
-    Serial.print(holdingRegs[2]);
+    holdingRegs[0] = 0;
+  }
 
-    Serial.print(" TICK=");
-    Serial.println(encoderTicks);
+  else if (holdingRegs[0] == 2)
+  {
+    Serial.println("buka");
+
+    angkat.write(0);
+    delay(300);
+
+    grip.write(0);
+
+    holdingRegs[0] = 0;
   }
 }
